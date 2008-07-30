fa4ddaa0eb4c79aaf7a02335c0765aaff3d391a5
Richard M. Heiberger <rmh@temple.edu>
2008-07-30 00:00:00 +0000

    version 1.1-15

--- R/Projector.R
+++ R/Projector.R
@@ -1,38 +1,22 @@
 Projector <- function() {
-  command <-
-    'options(Rcmdr=list(
-           suppress.menus=TRUE,
-           RExcelSupport=TRUE,
-           plugins="RcmdrPlugin.HH",
-           log.font.size = 15,
-           log.width = 54,
-           log.height = 6, 
-           log.commands = TRUE,
-           output.height = 18,
-           console.output = FALSE, 
-           contrasts = c("contr.Treatment", "contr.poly"),
-           grab.focus = TRUE, 
-           double.click = TRUE,
-           sort.names = TRUE,
-           scale.factor = 1.4))'
-  doItAndPrint(command)
-
-  command <- 'trellis.par.set(list(
-                 superpose.symbol=list(pch=rep(16, length(trellis.par.get("superpose.symbol")$pch))),
-                 plot.symbol=list(pch=16)))'
-  doItAndPrint(command)
-
-  command <- 'par(pch=16)'
-  doItAndPrint(command)
-
-  command <- '{
-tkmessageBox("-message", "Click the Blue R on the Excel Rcmdr menu\nRCommander: Update menus")
-putRcmdr("autoRestart", TRUE)
-closeCommander(ask=FALSE)
-Commander()
-}'
-  if (!is.na(options()$Rcmdr$RExcelSupport) & options()$Rcmdr$RExcelSupport)
-    doItAndPrint(command)
+  Rcmdr <- options()$Rcmdr
+  Projector.options <-
+    list(log.font.size = 15,
+         log.width = 54,
+         log.height = 6, 
+         output.height = 18,
+         scale.factor = 1.4)
+  Rcmdr[names(Projector.options)] <- Projector.options
+  options(Rcmdr=Rcmdr)
+  trellis.par.set(list(superpose.symbol=list(
+                         pch=rep(16,
+                           length(trellis.par.get("superpose.symbol")$pch))),
+                       plot.symbol=list(pch=16)))
+  par(pch=16)
+  
+  putRcmdr("autoRestart", TRUE)
+  closeCommander(ask=FALSE)
+  Commander()
 }
 
 ## source("~/HH-R.package/RcmdrPlugin.HH/R/Projector.R")
